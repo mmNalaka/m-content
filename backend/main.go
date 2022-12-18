38e0cc6f11f241598e57a19058f36745b6342474
mmNalaka <nalaka.manathunga@gmail.com>
2022-12-18 20:36:12 +0100

    chore(setup): initial  project setup

---
+++ backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+
+	database.ConnectDB()
+
+	router.SetupRoutes(app)
+	log.Fatal(app.Listen(":3000"))
+}
---
+++ backend/router/router.go
@@ -0,0 +1,61 @@
+package router
+
+import "github.com/gofiber/fiber/v2"
+
+func SetupAppRoutes(app *fiber.App) {
+	// app routes
+	app.Get("/health", PlaceholderHandler)
+	app.Get("/version", PlaceholderHandler)
+	app.Get("/ready", PlaceholderHandler)
+
+	// api v1 routes
+	v1 := app.Group("/api/v1")
+
+	// auth routers
+	auth := v1.Group("/auth")
+	auth.Post("/login", PlaceholderHandler)
+	auth.Post("/register", PlaceholderHandler)
+	auth.Post("/logout", PlaceholderHandler)
+	auth.Post("/refresh", PlaceholderHandler)
+	auth.Post("/userinfo", PlaceholderHandler)
+	auth.Post("/verify", PlaceholderHandler)
+	auth.Post("/change-password", PlaceholderHandler)
+	auth.Post("/forgot-password", PlaceholderHandler)
+	auth.Post("/reset-password/done", PlaceholderHandler)
+
+	// Protected routes
+	cda := v1.Group("/cda", ApiKeyProtected())
+	cda.Get("/collections", PlaceholderHandler)
+	cda.Get("/collections/:id", PlaceholderHandler)
+
+	// Protected routes
+	cma := v1.Group("/cma", JWTProtected())
+	cma.Get("/collections", PlaceholderHandler)
+	cma.Get("/collections/:id", PlaceholderHandler)
+	cma.Post("/collections", PlaceholderHandler)
+	cma.Put("/collections/:id", PlaceholderHandler)
+	cma.Delete("/collections/:id", PlaceholderHandler)
+}
+
+// dummy handlers
+func PlaceholderHandler(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"message": "Hello, World 👋!",
+		"path:":   c.Path(),
+		"method":  c.Method(),
+	})
+}
+
+// ApiKeyProtected dummy middleware
+func ApiKeyProtected() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		return c.Next()
+	}
+}
+
+// JWT protected dummy middleware
+func JWTProtected() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		return c.Next()
+	}
+}
